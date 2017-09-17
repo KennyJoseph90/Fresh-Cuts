2ed509e54e3eb3b1076a9370f69aeb68b6fc0b17
williamthein <sunsaying@gmail.com>
2017-09-16 19:38:53 -0700

    No-styling form and created js.file

---
+++ Fresh Cuts/js/main.js
@@ -0,0 +1,23 @@
+//Form
+
+var userName = document.getElementById('username');
+var	phone	 = document.getElementById('number');
+var	email	 = document.getElementById('email');
+var	service	 = document.getElementById('service');
+var	send	 = document.getElementById('send');
+var	output	 = document.getElementById('output');
+
+send.onclick = formValue;
+
+function formValue(){
+
+	var Name = userName.value;
+	var Phone= phone.value;
+	var Email= email.value;
+	var Service = service.value;
+
+	output.innerHTML = "Thank you for booking with us " + Name + "." + "Your number is " + Phone + " and the service that you have chosen is " + Service + "."; 
+
+
+	return false;
+};
